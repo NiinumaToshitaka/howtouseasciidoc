50c7d759533c26b49b66814e86bda0f36b166cc3
toshi <lysander.toshi@gmail.com>
2022-07-16 10:59:27 +0900

    コミットのハッシュ値と日時を文書に埋め込む
    adocファイルから生成したhtmlファイルやpdfファイルはgitで管理しない。
    このため，生成したファイルがどのバージョンを基にしたか明示するためには，
    ファイルを生成した日時よりもコミットのハッシュ値と日時を埋め込んだほうが確実。

--- build_html.sh
+++ build_html.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# HEADのコミットのハッシュ値
+head_commit_hash=`git rev-parse --short HEAD`
+# HEADのコミットの日時
+head_commit_date=`git log -1 --format='%cI' "${head_commit_hash}"`
+
 # HTML形式に変換する。
-asciidoctor -r asciidoctor-diagram --trace asciidoc_install_manual.adoc
+# HEADのコミットのハッシュ値と日時を指定して，文書中で参照できるようにする
+asciidoctor -r asciidoctor-diagram --trace asciidoc_install_manual.adoc -acommit-hash="${head_commit_hash}" -acommit-date="${head_commit_date}"
