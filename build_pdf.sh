90e863a5a337afffab585c223f64cb39da709c25
toshi <lysander.toshi@gmail.com>
2022-08-14 09:22:19 +0900

    コミットのハッシュ値と日時を文書に埋め込む
    commit: 50c7d7 と同様。
    PDFファイルを生成するスクリプトでコミットのハッシュ値と日時を埋め込んでいなかったので追加。

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# HEADのコミットのハッシュ値
+head_commit_hash=`git rev-parse --short HEAD`
+# HEADのコミットの日時
+head_commit_date=`git log -1 --format='%cI' "${head_commit_hash}"`
+
 # PDF形式に変換する。
-asciidoctor-pdf -r asciidoctor-diagram -r ./config.rb --trace asciidoc_install_manual.adoc
+asciidoctor-pdf -r asciidoctor-diagram -r ./config.rb --trace --verbose asciidoc_install_manual.adoc  -acommit-hash="${head_commit_hash}" -acommit-date="${head_commit_date}"
